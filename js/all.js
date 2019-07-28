34512e18cf9aa8452ab5a13c64c51d594b772b39
agrithel1009 <42888713+agrithel1009@users.noreply.github.com>
2019-07-28 13:31:05 +0800

    team

--- js/all.js
+++ js/all.js
@@ -67,14 +67,81 @@ $(document).ready(function() {
 		$('.lion1').toggle();
 		$('.lion2').toggle();
 	});
+	$('#team1').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team1_intro').siblings().hide();
+		$('.team1_intro').fadeIn(500);
+	});
+	$('#team2').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team2_intro').siblings().hide();
+		$('.team2_intro').fadeIn(500);
+	});
+	$('#team3').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team3_intro').siblings().hide();
+		$('.team3_intro').fadeIn(500);
+	});
 	$('#team4').click(function(){
 		event.preventDefault();
 		$('.team-menu').hide();
+		$('.team4_intro').siblings().hide();
 		$('.team4_intro').fadeIn(500);
+	});
+	$('#team5').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team5_intro').siblings().hide();
+		$('.team5_intro').fadeIn(500);
+	});
+	$('#team6').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team6_intro').siblings().hide();
+		$('.team6_intro').fadeIn(500);
+	});
+	$('#team7').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team7_intro').siblings().hide();
+		$('.team7_intro').fadeIn(500);
+	});
+	$('#team8').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team8_intro').siblings().hide();
+		$('.team8_intro').fadeIn(500);
+	});
+	$('#team9').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team9_intro').siblings().hide();
+		$('.team9_intro').fadeIn(500);
+	});
+	$('#team10').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team10_intro').siblings().hide();
+		$('.team10_intro').fadeIn(500);
+	});
+	$('#team11').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team11_intro').siblings().hide();
+		$('.team11_intro').fadeIn(500);
+	});
+	$('#team12').click(function(){
+		event.preventDefault();
+		$('.team-menu').hide();
+		$('.team12_intro').siblings().hide();
+		$('.team12_intro').fadeIn(500);
 	});
 	$('.back').click(function(){
 		event.preventDefault();
-		$('.team4_intro').hide();
+		$(this).parent().parent().hide();
 		$('.team-menu').fadeIn(500);
 	});
 });
