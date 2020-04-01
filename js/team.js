14882e09c05d53e00ef016a072583f64a916fff9
agrithel1009 <42888713+agrithel1009@users.noreply.github.com>
2020-04-01 17:01:02 +0800

    0401
    小組網頁漢堡選單修正
    刪除event.preventDefault();

--- js/team.js
+++ js/team.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
 	$('.team_intro').ready(function(){
 		$('.team_intro').fadeIn(1000);
 	});
+	$('.ham').click(function() {
+		$('#topmenu').slideToggle(600);
+	});
+	$('#topmenu a').click(function() {
+		$('#topmenu').slideUp(0);
+	});
+	$('.rightmenu a, #topmenu a').click(function(){
+		var target = $(this).attr('href');
+		var targetPos = $(target).offset().top;
+		$('html,body').animate({scrollTop: targetPos}, 1600);
+	});
 	$('.bye').ready(function(){
 		$('.bye').fadeIn(1000);
 	});
